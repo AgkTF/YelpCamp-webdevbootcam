313cc1a53d6a8fff79104591d637c4963358ac01
agkz1st <ahmed.agk22@gmail.com>
2018-08-26 23:49:47 +0200

    add process.env.PORT to app.listen

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use(indexRoutes);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("The YelpCamp server has started!");
 });
