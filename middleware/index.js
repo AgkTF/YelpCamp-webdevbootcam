4fa9356dcad5b1d8c4c8dbb11783dedefdb20952
agkz1st <ahmed.agk22@gmail.com>
2018-09-05 01:18:47 +0200

    Add code for admin role

--- middleware/index.js
+++ middleware/index.js
@@ -12,7 +12,7 @@ middlewareObj.checkCampgroundOwnership = function (req, res, next) {
                 res.redirect("back");
             } else {
                 //check if the user owns the campground
-                if (foundCampground.author.id.equals(req.user._id)) {
+                if (foundCampground.author.id.equals(req.user._id) || req.user.isAdmin) {
                     next();
                 } else {
                     req.flash("error", "You don't have permission to do that");
@@ -36,7 +36,7 @@ middlewareObj.checkCommentOwnership = function (req, res, next) {
                 res.redirect("back");
             } else {
                 //check if the user owns the comment
-                if (foundComment.author.id.equals(req.user._id)) {
+                if (foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
                     next(); // this next() runs the route handler.
                 } else {
                     req.flash("error", "You don't have permission to do that");
